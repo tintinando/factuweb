b4cb9f69f5fe99cb74be9e9866fd94414c8ab41b
Martin Andonegui <tintinando@gmail.com>
2026-07-29 17:35:50 -0300

    add kvnamespace

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -1,7 +1,8 @@
-type Bindings = {
-    CERT_PEM: string,
-    CUIT: string,
-    ENVIRONMENT: string,
+export type Bindings = {
+    AFIP_CACHE: KVNamespace;
+    ENVIRONMENT: string
+    CERT_PEM: string
+    CUIT: string
     PRIVATE_KEY: string
     API_TOKEN: string
 }
