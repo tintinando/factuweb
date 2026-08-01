8fe33ee355b249e8a61523b1f9a0e38b65e5316c
Martín Andonegui <tintinando@gmail.com>
2026-08-01 00:15:53 -0300

    feat: implementar funciones para construir solicitudes WSFE y definir tipos de datos

--- src/Arca/xmlBuilders/buildWsfeRequest.ts
+++ src/Arca/xmlBuilders/buildWsfeRequest.ts
@@ -1,7 +1,140 @@
-interface BuildWsfeRequestProps {}
+import { BuildWsfeRequestProps, WsfeDetalle } from './types';
 
-export function buildWsfeRequest({}: BuildWsfeRequestProps) {
-  const template = /* HTML */ `
+function renderDetalle(det: WsfeDetalle): string {
+  return /* HTML */ `
+    <ar:FECAEDetRequest>
+      <ar:Concepto>${det.concepto}</ar:Concepto>
+      <ar:DocTipo>${det.docTipo}</ar:DocTipo>
+      <ar:DocNro>${det.docNro}</ar:DocNro>
+      <ar:CbteDesde>${det.cbteDesde}</ar:CbteDesde>
+      <ar:CbteHasta>${det.cbteHasta}</ar:CbteHasta>
+      <ar:CbteFch>${det.cbteFch}</ar:CbteFch>
+      <ar:ImpTotal>${det.impTotal}</ar:ImpTotal>
+      <ar:ImpTotConc>${det.impTotConc}</ar:ImpTotConc>
+      <ar:ImpNeto>${det.impNeto}</ar:ImpNeto>
+      <ar:ImpOpEx>${det.impOpEx}</ar:ImpOpEx>
+      <ar:ImpTrib>${det.impTrib}</ar:ImpTrib>
+      <ar:ImpIVA>${det.impIVA}</ar:ImpIVA>
+      ${det.fchServDesde
+        ? `<ar:FchServDesde>${det.fchServDesde}</ar:FchServDesde>`
+        : ''}
+      ${det.fchServHasta
+        ? `<ar:FchServHasta>${det.fchServHasta}</ar:FchServHasta>`
+        : ''}
+      ${det.fchVtoPago
+        ? `<ar:FchVtoPago>${det.fchVtoPago}</ar:FchVtoPago>`
+        : ''}
+      <ar:MonId>${det.monId}</ar:MonId>
+      <ar:MonCotiz>${det.monCotiz}</ar:MonCotiz>
+      ${det.canMisMonExt
+        ? `<ar:CanMisMonExt>${det.canMisMonExt}</ar:CanMisMonExt>`
+        : ''}
+      ${det.condicionIVAReceptorId
+        ? `<ar:CondicionIVAReceptorId>${det.condicionIVAReceptorId}</ar:CondicionIVAReceptorId>`
+        : ''}
+      ${det.cbtesAsoc && det.cbtesAsoc.length > 0
+        ? `<ar:CbtesAsoc>
+              ${det.cbtesAsoc
+                .map(
+                  (c) => `
+                <ar:CbteAsoc>
+                  <ar:Tipo>${c.tipo}</ar:Tipo>
+                  <ar:PtoVta>${c.ptoVta}</ar:PtoVta>
+                  <ar:Nro>${c.nro}</ar:Nro>
+                  ${c.cuit ? `<ar:Cuit>${c.cuit}</ar:Cuit>` : ''}
+                  ${c.cbteFch ? `<ar:CbteFch>${c.cbteFch}</ar:CbteFch>` : ''}
+                </ar:CbteAsoc>`
+                )
+                .join('')}
+            </ar:CbtesAsoc>`
+        : ''}
+      ${det.tributos && det.tributos.length > 0
+        ? `<ar:Tributos>
+              ${det.tributos
+                .map(
+                  (t) => `
+                <ar:Tributo>
+                  <ar:Id>${t.id}</ar:Id>
+                  ${t.desc ? `<ar:Desc>${t.desc}</ar:Desc>` : ''}
+                  <ar:BaseImp>${t.baseImp}</ar:BaseImp>
+                  <ar:Alic>${t.alic}</ar:Alic>
+                  <ar:Importe>${t.importe}</ar:Importe>
+                </ar:Tributo>`
+                )
+                .join('')}
+            </ar:Tributos>`
+        : ''}
+      ${det.iva && det.iva.length > 0
+        ? `<ar:Iva>
+              ${det.iva
+                .map(
+                  (i) => `
+                <ar:AlicIva>
+                  <ar:Id>${i.id}</ar:Id>
+                  <ar:BaseImp>${i.baseImp}</ar:BaseImp>
+                  <ar:Importe>${i.importe}</ar:Importe>
+                </ar:AlicIva>`
+                )
+                .join('')}
+            </ar:Iva>`
+        : ''}
+      ${det.opcionales && det.opcionales.length > 0
+        ? `<ar:Opcionales>
+              ${det.opcionales
+                .map(
+                  (o) => `
+                <ar:Opcional>
+                  <ar:Id>${o.id}</ar:Id>
+                  <ar:Valor>${o.valor}</ar:Valor>
+                </ar:Opcional>`
+                )
+                .join('')}
+            </ar:Opcionales>`
+        : ''}
+      ${det.compradores && det.compradores.length > 0
+        ? `<ar:Compradores>
+              ${det.compradores
+                .map(
+                  (co) => `
+                <ar:Comprador>
+                  <ar:DocTipo>${co.docTipo}</ar:DocTipo>
+                  <ar:DocNro>${co.docNro}</ar:DocNro>
+                  <ar:Porcentaje>${co.porcentaje}</ar:Porcentaje>
+                </ar:Comprador>`
+                )
+                .join('')}
+            </ar:Compradores>`
+        : ''}
+      ${det.periodoAsoc
+        ? `<ar:PeriodoAsoc>
+              <ar:FchDesde>${det.periodoAsoc.fchDesde}</ar:FchDesde>
+              <ar:FchHasta>${det.periodoAsoc.fchHasta}</ar:FchHasta>
+            </ar:PeriodoAsoc>`
+        : ''}
+      ${det.actividades && det.actividades.length > 0
+        ? `<ar:Actividades>
+              ${det.actividades
+                .map(
+                  (a) => `
+                <ar:Actividad>
+                  <ar:Id>${a.id}</ar:Id>
+                </ar:Actividad>`
+                )
+                .join('')}
+            </ar:Actividades>`
+        : ''}
+    </ar:FECAEDetRequest>
+  `;
+}
+
+export function buildWsfeRequest({
+  auth,
+  header,
+  detalles
+}: BuildWsfeRequestProps): string {
+  const cantReg = detalles.length;
+
+  return /* HTML */ `
     <soapenv:Envelope
       xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
       xmlns:ar="http://ar.gov.afip.dif.FEV1/"
@@ -10,85 +143,18 @@ export function buildWsfeRequest({}: BuildWsfeRequestProps) {
       <soapenv:Body>
         <ar:FECAESolicitar>
           <ar:Auth>
-            <ar:Token>string</ar:Token>
-            <ar:Sign>string</ar:Sign>
-            <ar:Cuit>long</ar:Cuit>
+            <ar:Token>${auth.token}</ar:Token>
+            <ar:Sign>${auth.sign}</ar:Sign>
+            <ar:Cuit>${auth.cuit}</ar:Cuit>
           </ar:Auth>
           <ar:FeCAEReq>
             <ar:FeCabReq>
-              <ar:CantReg>int</ar:CantReg>
-              <ar:PtoVta>int</ar:PtoVta>
-              <ar:CbteTipo>int</ar:CbteTipo>
+              <ar:CantReg>${cantReg}</ar:CantReg>
+              <ar:PtoVta>${header.ptoVenta}</ar:PtoVta>
+              <ar:CbteTipo>${header.cbteTipo}</ar:CbteTipo>
             </ar:FeCabReq>
             <ar:FeDetReq>
-              <ar:FECAEDetRequest>
-                <ar:Concepto>int</ar:Concepto>
-                <ar:DocTipo>int</ar:DocTipo>
-                <ar:DocNro>long</ar:DocNro>
-                <ar:CbteDesde>long</ar:CbteDesde>
-                <ar:CbteHasta>long</ar:CbteHasta>
-                <ar:CbteFch>string</ar:CbteFch>
-                <ar:ImpTotal>double</ar:ImpTotal>
-                <ar:ImpTotConc>double</ar:ImpTotConc>
-                <ar:ImpNeto>double</ar:ImpNeto>
-                <ar:ImpOpEx>double</ar:ImpOpEx>
-                <ar:ImpTrib>double</ar:ImpTrib>
-                <ar:ImpIVA>double</ar:ImpIVA>
-                <ar:FchServDesde>string</ar:FchServDesde>
-                <ar:FchServHasta>string</ar:FchServHasta>
-                <ar:FchVtoPago>string</ar:FchVtoPago>
-                <ar:MonId>string</ar:MonId>
-                <ar:MonCotiz>double</ar:MonCotiz>
-                <ar:CanMisMonExt>string</ar:CanMisMonExt>
-                <ar:CondicionIVAReceptorId>int</ar:CondicionIVAReceptorId>
-                <ar:CbtesAsoc>
-                  <ar:CbteAsoc>
-                    <ar:Tipo>short</ar:Tipo>
-                    <ar:PtoVta>int</ar:PtoVta>
-                    <ar:Nro>Long</ar:Nro>
-                    <ar:Cuit>String</ar:Cuit>
-                    <ar:CbteFch>String</ar:CbteFch>
-                  </ar:CbteAsoc>
-                </ar:CbtesAsoc>
-                <ar:Tributos>
-                  <ar:Tributo>
-                    <ar:Id>short</ar:Id>
-                    <ar:Desc>string</ar:Desc>
-                    <ar:BaseImp>double</ar:BaseImp>
-                    <ar:Alic>double</ar:Alic>
-                    <ar:Importe>double</ar:Importe>
-                  </ar:Tributo>
-                </ar:Tributos>
-                <ar:Iva>
-                  <ar:AlicIva>
-                    <ar:Id>short</ar:Id>
-                    <ar:BaseImp>double</ar:BaseImp>
-                    <ar:Importe>double</ar:Importe>
-                  </ar:AlicIva>
-                </ar:Iva>
-                <ar:Opcionales>
-                  <ar:Opcional>
-                    <ar:Id>string</ar:Id>
-                    <ar:Valor>string</ar:Valor>
-                  </ar:Opcional>
-                </ar:Opcionales>
-                <ar:Compradores>
-                  <ar:Comprador>
-                    <ar:DocTipo>int</ar:DocTipo>
-                    <ar:DocNro>Long</ar:DocNro>
-                    <ar:Porcentaje>double</ar:Porcentaje>
-                  </ar:Comprador>
-                </ar:Compradores>
-                <ar:PeriodoAsoc>
-                  <ar:FchDesde>string</ar:FchDesde>
-                  <ar:FchHasta>string</ar:FchHasta>
-                </ar:PeriodoAsoc>
-                <ar:Actividades>
-                  <ar:Actividad>
-                    <ar:Id>Long</ar:Id>
-                  </ar:Actividad>
-                </ar:Actividades>
-              </ar:FECAEDetRequest>
+              ${detalles.map((det) => renderDetalle(det)).join('')}
             </ar:FeDetReq>
           </ar:FeCAEReq>
         </ar:FECAESolicitar>
