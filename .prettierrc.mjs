e00dcea6f2e52f60f878864d3de0ad298a7a7168
Martín Andonegui <tintinando@gmail.com>
2026-08-01 15:37:45 -0300

    prettier msj

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+export default {
+    "semi": true,
+    "singleQuote": true,
+    "tabWidth": 2,
+    "htmlWhitespaceSensitivity": "css",
+    "bracketSameLine": false,
+    "trailingComma": "none",
+    "printWidth": 80
+  }  
